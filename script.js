30a9d2da76ea4d024705021f89605b3e765fead5
jirkaroz <jiri.roznovjak@gmail.com>
2014-10-11 00:40:05 -0400

    scenes implemented

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ function sketchProc(processing) {
 var CWIDTH = 800; //canvas width and height
 var CHEIGHT = 600;
 var KEY = 0; // current processing.key pressed. for some reason, processing js doesn't allow to use boolean variable processing.keypressed, so I have to handle it on my own. 0 signifies no processing.key is pressed.
+var SCENE = 0;
 processing.size(CWIDTH,CHEIGHT);
 processing.background(72,208,235);
 var xTranslate = 0; //how much the scene is translated in the x-direction
-var smokes = []; //smoke behind the airplane
 var smokeTime = 0; //last time smoke was deployed
 var sceneOffset = 100; //offset for scene shifting
-var rings = [];
-var grounds = [];
 var groundLevel = 50; //offset of the ground from the bottom
 var leftMostGround = 400; 
 var rightMostGround = 400; //x-coordinate of the leftmost and rightmost ground
@@ -24,7 +22,7 @@ var Ground = function (xCenter) { //ground function, later will be animated
 
 Ground.prototype.display = function () {
 	processing.noStroke();
-	processing.fill(49,247,49);
+	processing.fill(131,247,73);
 	processing.rect(this.xCenter-CWIDTH/2,CHEIGHT-groundLevel,this.xCenter+CWIDTH/2,CHEIGHT);
 };
 
@@ -51,7 +49,7 @@ Ring.prototype.checkThrough = function (plane) { //returns true if airplane is i
 	}
 };
 
-var controls = function () {
+var controls = function (airplane) {
 	if (KEY === 37 || KEY === 38) {
 		airplane.turn(-1);
 	} else if (KEY === 39 || KEY === 40) {
@@ -83,7 +81,7 @@ Smoke.prototype.display = function () {
 	processing.ellipse(this.position.x,this.position.y,this.radius*2,this.radius*2);
 };
 
-var updateSmokes = function () { //function that gets called in the draw method and handles all the smoke stuff
+var updateSmokes = function (smokes,airplane) { //function that gets called in the draw method and handles all the smoke stuff
 	if (processing.millis() - smokeTime > 200) {
 		smokeTime = processing.millis();
 		smokes.push(new Smoke(airplane.position.x,airplane.position.y));
@@ -106,15 +104,17 @@ var Airplane = function (x,y) {
 	this.velocity = new processing.PVector(0,0); //needed to compute screen shifting
 	this.angle = 0; //declination from the X axis in radians
 	this.acceleration = 0;
-	this.angleMag = 0.03; //magnitude with which the plane will be turning
-	this.velRange = 1;
+	this.angleMag = 0.04; //magnitude with which the plane will be turning
+	this.velRange = 0.8;
 	this.minVel = 3;
+	this.isFlying = 1; //multiplies the velocitymag, is set to 0 when crashed
 };
 
 Airplane.prototype.update = function () {
 	//updates current position according to current velocity magnitude and angle using trig
 	this.velocityMag = Math.sin(this.angle)*this.velRange + this.minVel; //velocity is a function of the angle (up-slower,down-faster)
 	this.velocityMag += this.acceleration;
+	this.velocityMag *= this.isFlying;
 	this.velocity.x = this.velocityMag * Math.cos(this.angle);
 	this.velocity.y = this.velocityMag * Math.sin(this.angle);
 	this.position.add(this.velocity);
@@ -151,6 +151,7 @@ Airplane.prototype.run = function () { //gets called in the draw method, handles
 		this.takeOff();
 	}
 	this.checkUp();
+	this.checkGround();
 	this.update();
 	this.display();
 	
@@ -167,41 +168,75 @@ Airplane.prototype.checkUp = function () { //function that handles when airplane
 	}
 };
 
-
-var airplane = new Airplane(CWIDTH/2,540);
-var x = 0;
-var y = 0;
-for (var i=0;i<5;i++) {
-	x = Math.random()*760+20;
-	y = Math.random()*500+30;
-	rings.push(new Ring(x,y,30));
-}
-grounds.push(new Ground(400));
-
-processing.draw = function () { //what gets called before the shift scene stays the same and what after, gets shifted
-	processing.background(72,208,235);
-	grounds[0].display();
-	shiftScene(airplane);
-	controls();
-	updateSmokes();
-	
-	/*if (airplane.position.x > rightMostGround) { left checking partially not working, because rectangle apparently cant have + and - x coord.
-		grounds.push(new Ground(rightMostGround+800)); zatim to tak nech byt, kdyztak te pak neco napadne. Ted se bude zem posouvat se scenou.
-		rightMostGround += 800;
+Airplane.prototype.checkGround = function () { //checks if the airplane crashed to the ground
+	if (this.position.y > CHEIGHT-groundLevel) {
+		this.crash();
 	}
-	if (airplane.position.x < leftMostGround) {
-		grounds.push(new Ground(leftMostGround-800));
-		leftMostGround -= 800;
-	}*/
-	
-	
-	for (var i=0;i<rings.length;i++) {
-		rings[i].display();
-		if (rings[i].checkThrough(airplane)) {
-			rings[i].opacity = 0;
+};
+
+Airplane.prototype.crash = function () { //crashes the airplane (needs further implementation)
+	this.isFlying = 0;
+};
+
+var GameScene = function () {
+};
+
+GameScene.prototype.setup = function () {
+	this.aircraft = new Airplane(CWIDTH/2,540);
+	this.rings = [];
+	this.grounds = [new Ground(400)];
+	this.smokes = [];
+	var x = 100;
+	var y = 0;
+	for (var i=0;i<5;i++) {
+		//x = Math.random()*760+20;
+		y = Math.random()*500+30;
+		this.rings.push(new Ring(x,y,30));
+		x+=100;
+	}
+};
+
+GameScene.prototype.run = function () {
+	processing.background(72,208,235);
+	this.grounds[0].display();
+	shiftScene(this.aircraft);
+	controls(this.aircraft);
+	updateSmokes(this.smokes,this.aircraft);
+	for (var i=0;i<this.rings.length;i++) {
+		this.rings[i].display();
+		if (this.rings[i].checkThrough(this.aircraft)) {
+			this.rings[i].opacity = 0;
 		}
 	};
-	airplane.run();
+	this.aircraft.run();
+};
+
+var scene0 = function () {
+	processing.background(72,208,235);
+	processing.fill(255,255,255);
+	processing.textSize(50);
+	processing.text('Initial Scene',230,280);
+};
+
+var newscene = new GameScene();
+processing.draw = function () { //what gets called before the shift scene stays the same and what after, gets shifted
+	switch(SCENE) {
+		case 0:
+			scene0();
+			break;
+		case 1:
+			newscene.run();
+			break;
+	}
+};
+
+processing.mouseClicked = function () {
+	if (SCENE===0) {
+		newscene.setup();
+		SCENE=1;
+	} else if (SCENE===1) {
+		SCENE=0;
+	}
 };
 
 processing.keyPressed = function () {
@@ -211,7 +246,6 @@ processing.keyPressed = function () {
 processing.keyReleased = function () { //Key is reset to 0 when released
 	KEY = 0;
 };
-
 
 }
 var canvas = document.getElementById("canvas1");
